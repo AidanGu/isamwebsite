ed1123c4b252b73898e60eadb2745042fd4d070b
Aidan Gu <87115671+AidanGu@users.noreply.github.com>
2025-07-15 11:27:48 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter, Space_Grotesk } from "next/font/google"
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -39,7 +40,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={`${inter.variable} ${spaceGrotesk.variable} font-inter`}>{children}</body>
+      <body className={`${inter.variable} ${spaceGrotesk.variable} font-inter`}>{children}
+        <Analytics />
+      </body>
     </html>
   )
 }
